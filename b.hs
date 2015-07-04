e4330fb1c1f1cfe5550532c2b13e1f72aea3aa27
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-04 19:56:38 -0400

    Update

--- b.hs
+++ b.hs
@@ -2,33 +2,6 @@
 
 
 
-var aa:Int8 = 3
-
-var bb:Int16 = 3
-
-
-
-println("result = \(true)")
-// println("result = \(aa == bb)")	// not valid ?
-
-typealias Byte = UInt8
-
-
-
-var cc:Byte = 0xFF
-
-
-println("cc = \(cc)")
-
-
-if false {
-	println(cc)
-} else {
-	println(false)
-}
-
-var aaa = !(!false)	// whoah
-
 
 
 
