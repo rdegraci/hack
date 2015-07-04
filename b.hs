f12e7048a5b419f75363322817a48393143da696
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-04 19:58:56 -0400

    Update

--- b.hs
+++ b.hs
@@ -5,8 +5,8 @@
 typealias Byte = UInt8
 
 var tuple1 = (Byte(0xA), String("Hello"))
-var tuple = (Byte(0xA), String("Hello"))
+var tuple2 = (Byte(0xA), String("Hello"))
 
-print(tuple)
+print(tuple1)
 
 
