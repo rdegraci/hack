911dd2410b3f1e1d8c1830a86520807bcf07fc08
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-04 19:44:13 -0400

    Update

--- b.hs
+++ b.hs
@@ -1,39 +1,5 @@
 
 
-var min:UInt
-var max:UInt
-
-min = UInt.min
-max = UInt.max
-
-
-println("UInt min = \(min) and max = \(max)")
-
-
-var min64:UInt64
-var max64:UInt64
-
-min64 = UInt64.min
-max64 = UInt64.max
-
-
-println("UInt64 min = \(min64) and max = \(max64)")
-
-println(Int32)
-
-
-var aa:Float = 4
-
-
-var bb:Double = 4.0
-
-
-// var result = (aa == bb) // won't compile
-// println (result)	
-
-println( 4_000 == 4_000)
-
-println(false)
 
 
 
