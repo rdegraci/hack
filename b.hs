1b2d5ada67942423b2afde5fedb6c07a12e28d41
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-04 21:05:02 -0400

    Update

--- b.hs
+++ b.hs
@@ -17,4 +17,4 @@ let c = (a,b)
 
 // error: type '(T, inout TargetStream)' 
 // does not conform to protocol 'BooleanLiteralConvertible'
-//println(c.1 > 0 ? true : false)
+// println(c.1 > 0 ? true : false)
