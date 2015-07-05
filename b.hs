b23a3e47e2f7ab844008e639e7130fac577ae50d
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-04 20:02:47 -0400

    Update

--- b.hs
+++ b.hs
@@ -4,10 +4,10 @@
 
 typealias Byte = UInt8
 
-var (a,b) = (Byte(0xA), String("Hello"))
+var (a,_) = (Byte(0xA), String("Hello"))
 var tuple2 = (Byte(0xA), String("Hello"))
 
 
 println("a is \(a)")
 
-println("b is \(b)")
+//println("b is \(b)")
