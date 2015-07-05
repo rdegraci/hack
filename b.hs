489ab48bccdfe7451f5563a2cf3cbe349e1786e7
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-04 20:58:06 -0400

    Update

--- b.hs
+++ b.hs
@@ -1,40 +1,4 @@
 // compound assignment operators 
 
-var a:Int = 3
-
-let b = 	// HAH !!
-
-
-// missing argument for parameter #2 in call
-// println(++(a++))	
-
-println("a is \(a)")
-println("b is \(b)")
-
-
-// This is some fucked up bullshit
-// error: 'Int' is not identical to 'UInt8'
-// let c = ++(a++)
-
-
-var yy = 3
-println(++yy)
-
-// error: missing argument for parameter #2 in call
-// println((++yy)++)
-
-// println(++(++yy)) Illegal
-
-var cc:UInt8 = 3
-
-var c1 = --(cc)
-var c = ++(c1)
-
-// error: could not find an overload for '++' 
-// that accepts the supplied arguments
-// var c2 = ++(--(cc))
-
-
-println("cc is \(cc)")
 
 
