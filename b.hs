a7171cba590a5c8422e67435b3edccfd268bde8d
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-04 21:07:02 -0400

    Update

--- b.hs
+++ b.hs
@@ -18,6 +18,7 @@ let c = (a,b)
 // error: type '(T, inout TargetStream)' 
 // does not conform to protocol 'BooleanLiteralConvertible'
 // println(c.1 > 0 ? true : false)
+
 println(c.0)
 
 // Nil Coalescing Operator
