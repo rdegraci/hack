190c09f2d9796866bd0888a20279e17af0b52d11
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-06 23:39:03 -0400

    Update

--- b.hs
+++ b.hs
@@ -1,33 +1,10 @@
-// compound assignment operators 
 
 
 
-var a:UInt8 = 8
-a += 1
-
-println("a is \(a)")
 
 
-// 'UInt8' does not conform to protocol 'BooleanType'
-// println(a ? true : false)
 
 
-let b = "Hello"
-let c = (a,b)
-
-// error: type '(T, inout TargetStream)' 
-// does not conform to protocol 'BooleanLiteralConvertible'
-// println(c.1 > 0 ? true : false)
-
-println(c.0)
-
-// Nil Coalescing Operator
-println(nil ?? "hi!")
-
-
-let zz = (nil ?? "hi!")
-
-println(zz)
 
 
 
