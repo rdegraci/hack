3d1ebc69a27aed690fa1470ebc99c0be71d6944f
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-09 23:39:38 -0400

    Update

--- b.hs
+++ b.hs
@@ -18,7 +18,7 @@ println(c)
 // let d:Set<Int> = [1,3,4]
 // let d = Set([1,2,3,4])
 // var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
-// Excerpt From: Apple Inc. “The Swift Programming Language.” iBooks. https://itun.es/us/jEUH0.l
+
 
 
 
