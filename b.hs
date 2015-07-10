21e1bc6f165ead5d5817d594d3f2d1d1f072c09c
Rodney Degracia <rdegraci@elvenlabs.com>
2015-07-09 23:28:22 -0400

    Update

--- b.hs
+++ b.hs
@@ -2,56 +2,6 @@
 
 
 
-let a = 1...4
-
-println(a)
-
-
-let b = 1..<4
-
-println(b)
-
-
-for x in b {
- println(x)
-}
-
-
-for y in 1...4 {
-println(y)
-}
-
-
-let cc = ["a", "3"]
-
-println(cc.reverse())
-
-
-var z = "a"
-
-let s = String(z += "aa")
-
-println(s)
-
-
-for c in "Hello".utf8
-{
-  println(c)
-  }
-  
- 
-let greeting = "Hello World"
-for index in indices(greeting) {
-    print("\(greeting[index])! ")
-}
-println("\n")
-
-
-var ff = [Int]()
-ff.append(3)
-
-println(ff)
-
 
 
 
